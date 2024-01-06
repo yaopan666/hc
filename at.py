6f1d2a22bf2ce70a55a5906608378df64dd09622
yaopan666 <155442410+yaopan666@users.noreply.github.com>
2024-01-06 13:59:48 +0800

    Update at.py

--- at.py
+++ at.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.write('国金证券皇家量化团队为您服务，以下是我们的核心服务：一通乱吹')
-st.write('添加刘老师微信：www.cc.com')
+st.write('添加刘老师微信：www.baidu.com')
 st.write('AI投资助手：https://bm6zw6.aitianhu1.top/#/chat/1002')
 st.write('添加串串微信：yanhanlin,yaopan')
